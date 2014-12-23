6e563c5ea6474a698d4478f5952bbe6c3f552b2f
Jens Halm <halm@spicefactory.org>
2014-12-23 23:06:47 +0000

    add thread-safe session cache

--- connector/src/main/scala/com/sphonic/phantom/connector/DefaultSessionProvider.scala
+++ connector/src/main/scala/com/sphonic/phantom/connector/DefaultSessionProvider.scala
@@ -2,9 +2,13 @@ package com.sphonic.phantom.connector
 
 import com.datastax.driver.core.Session
 import com.datastax.driver.core.Cluster
+import scala.collection.concurrent.TrieMap
 
 class DefaultSessionProvider (builder: ClusterBuilder) extends SessionProvider {
 
+  
+  private val sessionCache = new Cache[String,Session]
+  
   
   lazy val cluster: Cluster = {
     // TODO - the original phantom modules had .withoutJMXReporting().withoutMetrics() as defaults, discuss best choices
@@ -19,12 +23,39 @@ class DefaultSessionProvider (builder: ClusterBuilder) extends SessionProvider {
     session
   }
   
-  def getSession (keySpace: String): Session = {
+  protected def createSession (keySpace: String): Session = {
     // TODO - the connect method might throw exceptions, decide on error handling
-    // TODO - cache sessions per keySpace
     val session = cluster.connect
     initKeySpace(session, keySpace)
   }
-    
   
+  def getSession (keySpace: String): Session = {
+    sessionCache.getOrElseUpdate(keySpace, createSession(keySpace))
+  }
+  
+}
+
+class Cache[K,V] {
+
+  /* this implementation uses putIfAbsent from the underlying TrieMap as
+   * getOrElseUpdate is not thread-safe. */
+  
+  private val map = TrieMap[K,Lazy]()
+  
+  private class Lazy (value: => V) {
+    lazy val get: V = value
+  }
+  
+  def getOrElseUpdate (key: K, op: => V): V = {
+    val lazyOp = new Lazy(op)
+    map.putIfAbsent(key, lazyOp) match {
+      case Some(oldval) =>
+        // don't evaluate the new lazyOp, return existing value
+        oldval.get
+      case _ =>
+        // no existing value for key, evaluate lazyOp
+        lazyOp.get
+    }
+  }
+
 }
