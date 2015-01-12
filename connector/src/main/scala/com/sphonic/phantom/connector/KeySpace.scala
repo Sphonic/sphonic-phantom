c4223d55165a7d3103c07510c12ce3213c8e2161
Jens Halm <halm@spicefactory.org>
2015-01-12 10:49:57 +0000

    expose session in KeySpace

--- connector/src/main/scala/com/sphonic/phantom/connector/KeySpace.scala
+++ connector/src/main/scala/com/sphonic/phantom/connector/KeySpace.scala
@@ -1,7 +1,11 @@
 package com.sphonic.phantom.connector
 
+import com.datastax.driver.core.Session
+
 class KeySpace (val name: String, val provider: SessionProvider) { outer =>
 
+  lazy val session: Session = provider.getSession(name)
+  
   trait Connector extends com.sphonic.phantom.connector.Connector {
     
     lazy val provider = outer.provider
