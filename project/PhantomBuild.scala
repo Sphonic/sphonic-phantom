52f684d66f918bc1e2584a2c703bc7861bc96b9f
Jens Halm <halm@spicefactory.org>
2015-01-13 08:59:55 +0000

    reduce log output of embedded Cassandra, bump version to 0.2.1

--- project/PhantomBuild.scala
+++ project/PhantomBuild.scala
@@ -45,7 +45,7 @@ object AnalyticsServer extends Build {
 
   val sharedSettings: Seq[Def.Setting[_]] = Seq(
     organization := "com.sphonic",
-    version := "0.2.0",
+    version := "0.2.1",
     scalaVersion := ScalaVersion,
     resolvers ++= Seq(
       "Typesafe repository"              at "http://repo.typesafe.com/typesafe/releases/",
