d978b66510cc1d0495e6da470aed1b2a74b2f333
Jens Halm <halm@spicefactory.org>
2015-01-13 20:27:18 +0000

    add scaladoc to zookeeper module

--- zookeeper/src/main/scala/com/sphonic/phantom/zookeeper/ZkContactPointLookup.scala
+++ zookeeper/src/main/scala/com/sphonic/phantom/zookeeper/ZkContactPointLookup.scala
@@ -12,6 +12,12 @@ import com.twitter.util.Duration
 import scala.collection.JavaConverters._
 import java.net.InetSocketAddress
 
+/**
+ * Performs the lookup of Cassandra nodes
+ * in Zookeeper. Implemented as function
+ * `Cluster.Builder => Cluster.Builder` so that it can
+ * be easily plugged into a `KeySpaceBuilder`.
+ */
 class ZkContactPointLookup (host: String, 
                             port: Int, 
                             path: String = "/cassandra", 
@@ -24,6 +30,10 @@ class ZkContactPointLookup (host: String,
   }
   
   
+  /**
+   * Exposes the instance of the Finagle
+   * Zookeeper API that is used to perform the lookup.
+   */
   protected lazy val zkClient: ZkClient = {
     val conn = s"$host:$port"
     val client = ZooKeeper.newRichClient(conn)
@@ -31,6 +41,10 @@ class ZkContactPointLookup (host: String,
     client
   }
   
+  /**
+   * Performs the actual lookup of Cassandra nodes
+   * in Zookeeper.
+   */
   protected def retrieveContactPoints: Future[Seq[InetSocketAddress]] = 
     zkClient.getData(path, watch = false) map {
       res => Try {
@@ -38,6 +52,12 @@ class ZkContactPointLookup (host: String,
       } getOrElse Seq.empty[InetSocketAddress]
     }
   
+  /**
+   * Parses the specified string into a sequence
+   * of InetSocketAddress instances. The default
+   * implementation expects a comma
+   * separated list of host/port pairs separated by a colon.
+   */
   protected def parseContactPoints(data: String): Seq[InetSocketAddress] = {
     data.split("\\s*,\\s*").map(_.split(":")).map {
       case Array(hostname, port) => new InetSocketAddress(hostname, port.toInt)
@@ -46,19 +66,54 @@ class ZkContactPointLookup (host: String,
   
 }
 
+/**
+ * Entry point for defining a keySpace based
+ * on looking up Cassandra nodes in Zookeeper.
+ * 
+ * The default Zookeeper path being used for the lookup
+ * is `/cassandra` and the expected format is a comma
+ * separated list of host/port pairs separated by a colon.
+ */
 object ZkContactPointLookup {
 
-    
+
+  /**
+   * Zookeeper's default port.
+   */
   val defaultPort = 2181
   
-  
+  /**
+   * A keyspace builder based on looking up
+   * contact points in Zookeeper running on the default
+   * port on localhost.
+   */
   lazy val local = apply(defaultPort)
 
+  /**
+   * A keyspace builder based on looking up
+   * contact points in Zookeeper running on the specified
+   * port on localhost.
+   */
   def apply (port: Int): KeySpaceBuilder = apply("localhost", port)
   
+  /**
+   * A keyspace builder based on looking up
+   * contact points in Zookeeper running on the specified
+   * host and port.
+   */
   def apply (host: String, port: Int): KeySpaceBuilder = 
     new KeySpaceBuilder(new ZkContactPointLookup(host,port))
   
+  /**
+   * A keyspace builder based on looking up
+   * contact points in Zookeeper running on the specified
+   * host and port.
+   * 
+   * @param host the host Zookeeper runs on
+   * @param port the port Zookeeper runs on
+   * @param path the Zookeeper path to use for looking up Cassandra nodes
+   * @param timeout the timeout for performing the lookup
+   */
   def apply (host: String, port: Int, path: String, timeout: Duration): KeySpaceBuilder = 
     new KeySpaceBuilder(new ZkContactPointLookup(host,port,path,timeout))
   
